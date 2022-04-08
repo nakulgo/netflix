9bb3e41b4e087d416e973fcb31a5a09aee1c2f72
Zara Goel <zaragoel@Zaras-MacBook-Pro.local>
2022-04-07 20:02:37 -0400

    final commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
+import React from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
-import Banner from "./Banner";
+import Banner from './Banner';
+import Nav from './Nav';
 
 function App() {
-  return (
-    <div className="App">
-      <Banner></Banner>
-      <Row 
-        title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchTopRated}
-        isLargeRow
-      />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}></Row> 
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}></Row>
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}></Row>
-      <Row title="Trending now" fetchUrl={requests.fetchTrending}></Row>
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}></Row>
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}></Row>
-      
-
-    </div>
-  );
+	return (
+		<div className="app">
+			<Nav />
+			<Banner />
+			<Row title="NETFLIX ORIGINALS" isLargeRow fetchUrl={requests.fetchRomanceMovies} />
+			<Row title="Trending" fetchUrl={requests.fetchTrending} />
+			<Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+			<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+			<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+		</div>
+	);
 }
 
 export default App;
--- src/Banner.js
+++ src/Banner.js
@@ -1,51 +1,59 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from './axios';
 import requests from './requests';
-import './banner.css';
+import "./Banner.css"
 
-function Banner(){
-    const [movie, setMovie] = useState([]);
+function Banner() {
+  const [movie, setMovie] = useState([]);
 
-    useEffect(() => {
-        async function fetchData(){
-            const request = await axios.get(requests.fetchTopRated)
-            setMovie(
-                request.data.results[
-                    Math.floor(Math.random() * request.data.results.length)
-                ]
-            );
-            return request;
-        }
-        fetchData()
-    }, []);
+  useEffect(() => {
 
-function truncate(str, n){
-    return str?.length > n ? str.substr(0,n-1) + "..." : str;
+    async function fetchData() {
 
-}
+      const request = await axios.get(requests.fetchDocumentaries);
+      setMovie(
+        request.data.results[
+        Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      // Math.floor(Math.random() * request.data.results.length -1)
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  // console.log(movie)
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
-    <header 
-    className="banner"
-    style={{
-        backgroundSize: 'cover',
-        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-        backgroundPosition: "center center",
-    }}
+    <header className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+        "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+        )`,
+        backgroundPosition: "center center"
+      }}
     >
-    <div className='banner__contents'>
-      {/* background image */}
-      <h1 className='banner__title'>
+      <div className="banner_contents">
+        <h1 className="banner_title">
           {movie?.title || movie?.name || movie?.original_name}
-      </h1>
-      <div className='banner__buttons'>
-          <button className='banner__button'>Play</button>
-          <button className='banner__button'>My List</button>
+        </h1>
+
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
-      <h1 className='banner__description'>{movie?.overview}</h1>
-      {" "}
-      </div>
+
+      <div className="banner--fadeBottom" />
     </header>
-  );
+
+  )
 }
 
-export default Banner;
+export default Banner
